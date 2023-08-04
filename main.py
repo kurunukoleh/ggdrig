bffd1582669b727ccc9766b7fa7c335be7f015ba
kurunukoleh <85008916+kurunukoleh@users.noreply.github.com>
2023-08-04 14:15:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#імпорт потрібних модулів
 from PyQt5.QtWidgets import *
 from random import randint
 
+#створення тексту та кнопок
 app = QApplication([])
 window = QWidget()
 window.resize(500 , 500)
@@ -9,6 +11,7 @@ text2 = QLabel('ви скібіді тоілет !!!!')
 text3 = QLabel('негри бравл старс')
 button = QPushButton('натисни і получи 23331 фіріспінів і 41254215 троянів на свій пк')
 
+#функції відповідяльні за дії кнопки при натисканні
 def knopka ():
     a = str(randint(1 , 100))
     text2.setText(a)
@@ -17,14 +20,18 @@ def knopka2 ():
     a = str(randint(1 , 100))
     text3.setText(a)
 
+#добавлення різних обєктів на лінію
 line = QVBoxLayout()
 line.addWidget(text1)
 line.addWidget(text2)
 line.addWidget(button)
 line.addWidget(text3)
 
+#відмалбовування лінії і підєднання функції до кнопки
 window.setLayout(line)
 button.clicked.connect(knopka)
 button.clicked.connect(knopka2)
+
+#показ вікна та оновлення кадрів
 window.show()
 app.exec()
